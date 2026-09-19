f89389831ad88ea17df512ba34cc05e146549d6c
Felipe Slavic <felipe.slavic2006@gmail.com>
2026-09-19 01:38:16 -0300

    Integra backend atualizado ao projeto final

---
+++ backend/src/main/java/br/fiap/daiji/config/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package br.fiap.daiji.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5501",
+                        "http://127.0.0.1:5501")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Accept");
+    }
+}
